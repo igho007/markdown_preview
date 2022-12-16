13017a369eb191de11ddb2e396ddc71e09c09c18
william okosun <williamokosun@gmail.com>
2022-12-16 06:34:30 +0100

    markdown-preview

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { marked } from "marked";
+import "bootstrap/dist/css/bootstrap.min.css";
+const App = () => {
+  marked.setOptions({ breaks: true });
+  const [text, setText] = useState(`
+  # H1
+  ## H2
+   [title](https://www.example.com)
+   1. First item
+   2. Second item
+  3. Third item
+  \`code\`
+  	> blockquote
+    	![alt text](image.jpg)
 
-function App() {
+\`\`\`
+{
+  "firstName": "John",
+  "lastName": "Smith",
+  "age": 25
+}
+\`\`\`
+**bold text**
+
+  `);
+  const handleChange = (e) => setText(e.target.value);
+
+  const markedText = marked(text);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row my-5">
+        <div className="col-md-4 mx-2">
+          <textarea
+            name="editor"
+            id="editor"
+            cols="30"
+            rows="10"
+            className="form-control"
+            onChange={handleChange}
+            value={text}
+          ></textarea>
+        </div>
+        <div
+          id="preview"
+          className="col-md-4 h-100 bg-info rounded-2"
+          dangerouslySetInnerHTML={{ __html: markedText }}
+        ></div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
